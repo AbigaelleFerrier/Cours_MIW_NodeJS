5b41063dbbfe6638b17003d56f7e042d49ef50c3
cirill <cirill@asheart.fr>
2019-12-06 10:32:35 +0100

    starter node

---
+++ 1_WebPack/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+export default () => {
+    try {
+        const app  = express();
+        const port = 3000;
+
+        app.listen(port, () => {
+            console.log('un truc');
+        })
+
+    } catch (e) {
+        console.error(e);
+    }
+}
